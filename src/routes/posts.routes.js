cda909f2302fb081f342301fe164faef53babe66
abhishektiwari-maker <abhishek.tiwari@kalvium.community>
2025-12-09 19:41:24 +0530

    refactor: standardize all API responses

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,10 +3,10 @@ const { getAllPosts, getPostById } = require("../controllers/posts.controller");
 
 const router = express.Router();
 
-// Get all posts
+
 router.get("/", getAllPosts);
 
-// Get single post by id
+
 router.get("/:postId", getPostById);
 
 module.exports = router;
