e99f8e28d406383514e0c2aacd0f9925611f4043
abhishektiwari-maker <abhishek.tiwari@kalvium.community>
2025-12-27 20:45:28 +0530

    created db layers

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -4,7 +4,7 @@ const postSchema = new mongoose.Schema(
 	{
 		title: { type: String, required: true },
 		content: { type: String, required: true },
-		author: { type: String, required: true },
+		author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
 		isDeleted: { type: Boolean, default: false },
 	},
 	{ timestamps: true }
